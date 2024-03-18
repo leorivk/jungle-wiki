3cea266d67bbd2a35f8fff450d345b36954045af
박현민 <dndb4902@naver.com>
2024-03-18 22:10:06 +0900

    # 로그인/회원가입 페이지

--- app.py
+++ app.py
@@ -7,5 +7,14 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/login")
+def loginPage():
+    return render_template("login.html")
+
+
+@app.route("/signup")
+def signUpPage():
+    return render_template("signup.html")
+
 if __name__ == '__main__':
     app.run(debug=True)

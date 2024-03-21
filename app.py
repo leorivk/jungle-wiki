67f5d0ed99fba3152139baec9eb4d6e6aa78672f
박현민 <dndb4902@naver.com>
2024-03-21 11:31:18 +0900

    # port upd

--- app.py
+++ app.py
@@ -35,4 +35,4 @@ app.context_processor(inject_template_globals)
 app.jinja_env.add_extension('jinja2.ext.loopcontrols')
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5001, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
